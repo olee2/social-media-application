9ba63d7594434dd4c8ca99baa647428ade7e814e
olee2 <65301179+olee2@users.noreply.github.com>
2022-10-09 13:32:13 +0200

    Updating register and login

--- dist/js/register.js
+++ dist/js/register.js
@@ -3,21 +3,19 @@ import { setLoader } from "./components/loader.mjs";
 import { errorHtml } from "./components/error.mjs";
 
 const form = document.querySelector("form");
-const user = document.querySelector(".user-name");
-const email = document.querySelector(".email");
-const password = document.querySelector(".password");
 const signalContainer = document.querySelector(".signal");
 
 form.onsubmit = async (e) => {
   e.preventDefault();
 
+  const form = e.target;
+
+  const formData = new FormData(form);
+  const profile = Object.fromEntries(formData.entries());
+
   const options = {
     method: "POST",
-    body: JSON.stringify({
-      name: user.value,
-      email: email.value,
-      password: password.value,
-    }),
+    body: JSON.stringify(profile),
     headers: { "Content-type": "application/json; charset=UTF-8" },
   };
 
