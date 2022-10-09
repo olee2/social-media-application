eb1b993f05f147b3b01a7eae28593e476a7dfb6f
olee2 <65301179+olee2@users.noreply.github.com>
2022-10-09 13:33:16 +0200

    Profile page and modify post

--- dist/js/profile.js
+++ dist/js/profile.js
@@ -1,7 +1,21 @@
 import { logOut } from "./components/log-out.mjs";
-import { loggedIn } from "./components/logged-in.mjs";
+import { profileHtml, postsHtml } from "./components/createHtml.mjs";
+import { setLoader } from "./components/loader.mjs";
+import { getUser } from "./components/storage.mjs";
+import { getPosts } from "./components/posts.mjs";
 
 const logOutBtn = document.querySelector(".log-out");
-const token = loggedIn();
+const profile = document.querySelector(".profile-details");
+const postContainer = document.querySelector(".post-container");
+const details = getUser();
+
+profile.innerHTML = setLoader();
 
 logOutBtn.onclick = () => logOut();
+
+profile.innerHTML = profileHtml(details);
+
+getPosts("desc").then((posts) => {
+  posts = posts.filter((post) => post.author.name === details.name);
+  postContainer.innerHTML = postsHtml(posts);
+});
